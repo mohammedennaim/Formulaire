fecdb50938771dc3eb84fd7210d94ed749873ea1
mohammedennaim <mohammedennaim99@egmail.com>
2024-10-30 10:13:56 +0100

    ajouter partie js pour validation

--- script.js
+++ script.js
@@ -4,50 +4,75 @@ const form3 = document.getElementById('form3');
 const nom = document.getElementById('nom');
 const prenom = document.getElementById('prenom');
 const age = document.getElementById('age');
+const tel = document.getElementById('telephone');
+const email = document.getElementById('email');
 const nextForm1 = document.getElementById('next-form-1');
 const nextForm2 = document.getElementById('next-form-2');
 const returnForm2 = document.getElementById('retour-form-2');
 const returnForm3 = document.getElementById('retour-form-3');
 
+message = 'champ is obligatoire'
+let groups;
 
-// const retour = document.getElementById('retour');
-// const save = document.getElementById('save');
+nextForm1.addEventListener("click", (e)=>{  
+const nomValue = nom.value.trim();
+const prenomValue = prenom.value.trim();
+const ageValue = age.value.trim();
 
-// form.addEventListener('click' , e=>{
-//     e.preventDefault();
-//     validationForm();
-// })
-
-const setErreur = (element,message) => {
-
-}
-const validationForm = () => {
-    const nomValue = nom.value.trim();
-    const prenomValue = prenom.value.trim();
-    const ageValue = age.value.trim();
-
-    if (nomValue === '') {
-        setErreur()
-    }
-    if (prenomValue === '') {
-        setErreur()
-    }
-    if (ageValue === '') {
-        setErreur()
-    }
-}
-
-nextForm1.addEventListener("click", (e)=>{
     e.preventDefault();
-    if(form1.style.display!="none"){
+    if (nomValue == '') {
+        groups = nom.parentElement
+        groups.querySelector(".erreur").innerText = message;
+        groups.querySelector(".erreur").style.color = "red";
+    }else{
+        groups = nom.parentElement;
+        groups.querySelector(".erreur").style.display = "none";
+    }
+    if (prenomValue == '') {
+        groups = prenom.parentElement;
+        groups.querySelector(".erreur").innerText = message;
+        groups.querySelector(".erreur").style.color = "red";
+    }else{
+        groups = prenom.parentElement;
+        groups.querySelector(".erreur").style.display = "none";
+    }
+    if (ageValue < 18) {
+        groups = age.parentElement;
+        groups.querySelector(".erreur").innerText = 'saisir age plus 18'
+        groups.querySelector(".erreur").style.color ="red"
+    }else{
+        groups = age.parentElement;
+        groups.querySelector(".erreur").innerText = ''
+    }
+    
+    if(form1.style.display!="none" && nomValue != '' && prenomValue != '' && ageValue != ''){
         form1.style.display = 'none'
         form2.style.display = 'flex'
     }
 })
 
 nextForm2.addEventListener("click", (e)=>{
+const telValue = tel.value.trim();
+const emailValue = email.value.trim();
     e.preventDefault();
-    if(form2.style.display!="none"){
+    if (telValue == '') {
+        groups = tel.parentElement
+        groups.querySelector(".erreur").innerText = message;
+        groups.querySelector(".erreur").style.color = "red";
+    }else{
+        groups = tel.parentElement;
+        groups.querySelector(".erreur").style.display = "none";
+    }
+    if (emailValue == '') {
+        groups = email.parentElement;
+        groups.querySelector(".erreur").innerText = message;
+        groups.querySelector(".erreur").style.color = "red";
+    }else{
+        groups = prenom.parentElement;
+        groups.querySelector(".erreur").style.display = "none";
+    }
+    
+    if(form2.style.display!="none" && telValue !='' && emailValue !=''){
         form2.style.display = 'none'
         form3.style.display = 'flex'
     }
