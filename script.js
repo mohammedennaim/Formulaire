01aca2214380e7b0cd5fa6a5245cafc5ff6bad2f
mohammedennaim <mohammedennaim99@egmail.com>
2024-10-31 23:02:35 +0100

    ajouter partie local storage

--- script.js
+++ script.js
@@ -6,26 +6,29 @@ const prenom = document.getElementById('prenom');
 const age = document.getElementById('age');
 const tel = document.getElementById('telephone');
 const email = document.getElementById('email');
-const feedback =document.getElementById('feedback');
+const feedback = document.getElementById('feedback');
 const save = document.getElementById('save');
 const nextForm1 = document.getElementById('next-form-1');
 const nextForm2 = document.getElementById('next-form-2');
 const returnForm2 = document.getElementById('retour-form-2');
 const returnForm3 = document.getElementById('retour-form-3');
+const afficher = document.getElementById('afficher');
+const affichage = document.getElementById('affichage');
 
-message = 'champ is obligatoire'
+message = 'champ is obligatoire';
 let groups;
 
-nextForm1.addEventListener("click", (e)=>{  
+nextForm1.addEventListener('click', (e)=>{  
 const nomValue = nom.value.trim();
 const prenomValue = prenom.value.trim();
 const ageValue = age.value.trim();
 
     e.preventDefault();
     if (nomValue == '') {
-        groups = nom.parentElement
+        groups = nom.parentElement;
         groups.querySelector(".erreur").innerText = message;
         groups.querySelector(".erreur").style.color = "red";
+        groups.querySelector(".erreur").style.display = "block";
     }else{
         groups = nom.parentElement;
         groups.querySelector(".erreur").style.display = "none";
@@ -34,6 +37,7 @@ const ageValue = age.value.trim();
         groups = prenom.parentElement;
         groups.querySelector(".erreur").innerText = message;
         groups.querySelector(".erreur").style.color = "red";
+        groups.querySelector(".erreur").style.display = "block";
     }else{
         groups = prenom.parentElement;
         groups.querySelector(".erreur").style.display = "none";
@@ -42,39 +46,57 @@ const ageValue = age.value.trim();
         groups = age.parentElement;
         groups.querySelector(".erreur").innerText = 'saisir age plus 18'
         groups.querySelector(".erreur").style.color ="red"
+        groups.querySelector(".erreur").style.display = "block";
     }else{
         groups = age.parentElement;
-        groups.querySelector(".erreur").innerText = ''
+        groups.querySelector(".erreur").style.display = "none";
     }
     
     if(form1.style.display!="none" && nomValue != '' && prenomValue != '' && ageValue > 17){
-        form1.style.display = 'none'
-        form2.style.display = 'flex'
+        form1.style.display = 'none';
+        form2.style.display = 'flex';
     }
 })
 
-nextForm2.addEventListener("click", (e)=>{
+
+// const regexTel = /^[0-9]-{10,}$/;
+
+nextForm2.addEventListener('click', (e)=>{
 const telValue = tel.value.trim();
 const emailValue = email.value.trim();
+const regexEmail = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/;
     e.preventDefault();
     if (telValue == '') {
         groups = tel.parentElement
         groups.querySelector(".erreur").innerText = message;
         groups.querySelector(".erreur").style.color = "red";
+        groups.querySelector(".erreur").style.display = "block";
     }else{
         groups = tel.parentElement;
         groups.querySelector(".erreur").style.display = "none";
     }
+
     if (emailValue == '') {
         groups = email.parentElement;
         groups.querySelector(".erreur").innerText = message;
         groups.querySelector(".erreur").style.color = "red";
+        groups.querySelector(".erreur").style.display = "block";
     }else{
-        groups = prenom.parentElement;
+        groups = email.parentElement;
         groups.querySelector(".erreur").style.display = "none";
     }
+
+    if (regexEmail.test(emailValue) == false) {
+        groups = email.parentElement;
+        groups.querySelector(".regx").innerText = 'email pas correct';
+        groups.querySelector(".regx").style.color = "red";
+        groups.querySelector(".regx").style.display = "block"
+    }else{
+        groups = email.parentElement;
+        groups.querySelector(".regx").style.display = "none";
+    }
     
-    if(form2.style.display!="none" && telValue !='' && emailValue !=''){
+    if(form2.style.display!="none" && telValue !='' && regexEmail.test(emailValue) && emailValue != ''){
         form2.style.display = 'none'
         form3.style.display = 'flex'
     }
@@ -95,15 +117,25 @@ returnForm3.addEventListener("click", (e)=>{
         form2.style.display = 'flex'
     }
 })
+
 save.addEventListener('click', (e) =>{
+    let formulaire = {
+        nom : nom.value,
+        prenom : prenom.value,
+        age : age.value,
+        telephone : tel.value,
+        email : email.value,
+        feedback : feedback.value
+    }
     const fbValue = feedback.value.trim();
     e.preventDefault();
     if(fbValue == ''){
-        groups =feedback.parentElement;
+        groups = feedback.parentElement;
         groups.querySelector('.erreur').innerText = message;
         groups.querySelector('.erreur').style.color ="red";
     }else{
         groups.querySelector('.erreur').style.display = "none";
-        alert("success");
+        localStorage.setItem('formulaire', JSON.stringify(formulaire));
+        
     }
 } )
